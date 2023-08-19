d0a9f2d72455e756b23b82f3df7d75d18c21ae3c
jasoncheng <funnyjason27@gmail.com>
2023-08-19 19:01:15 +0800

    onclick function scaffold

---
+++ tools/model-generator.js
@@ -0,0 +1,10 @@
+function onConvertToModel() {
+  // get the input from textarea with id = convertor-input
+  var input = document.getElementById('convertor-input').value;
+
+  // TODO: convert the input to model
+  var result = input;
+
+  // set the output to textarea with id = convertor-output
+  document.getElementById('convertor-output').value = result;
+}
